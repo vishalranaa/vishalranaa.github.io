efb64aaff070e7b539b10ab8bffeaf942586dfa9
vishalrana95 <vishalrajput9595@gmail.com>
2022-06-04 13:17:12 +0530

    build deploying

---
+++ _next/static/mJp7KgVqAupEuY6Xy-zyd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-88dafef620e52e5c1c53.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
