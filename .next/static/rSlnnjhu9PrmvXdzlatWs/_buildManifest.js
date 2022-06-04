ae1b2a10eea93f0301c4d28ebaf114d7dd0ced35
vishalrana95 <vishalrajput9595@gmail.com>
2022-06-04 13:34:23 +0530

    build

---
+++ .next/static/rSlnnjhu9PrmvXdzlatWs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-69af4ab7dd15f2dc9569.js"],"/_error":["static/chunks/pages/_error-1708848c533031db1ddd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
